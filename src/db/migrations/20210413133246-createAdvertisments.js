4e97d9d3338a3b685fae70f7262395c23d249a47
Yevhenko <yevhenkozlyi@gmail.com>
2021-04-13 16:42:23 +0300

    the project has been set up

---
+++ src/db/migrations/20210413133246-createAdvertisments.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable implicit-arrow-linebreak */
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Advertisements', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING(200),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING(1000),
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Advertisements'),
+};
